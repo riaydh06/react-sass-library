4a89b4afc301f494f3963db6661ef25f9a827f8a
Sayed Riaydh <$email>
2023-06-21 01:09:56 +0600

    feat: styling update

---
+++ build/Button/Button.stories.d.ts
@@ -0,0 +1,7 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "./index";
+import "../styles/styles.scss";
+declare const meta: Meta<typeof Button>;
+export default meta;
+type Story = StoryObj<typeof Button>;
+export declare const Primary: Story;
---
+++ build/index.d.ts
@@ -0,0 +1,2 @@
+export { default as TestComponent } from './TestComponent/TestComponent';
+export { default as Button } from './Button';
