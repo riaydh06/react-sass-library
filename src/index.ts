48f0c65b1b20252143029d4a60a313ac103dc359
Sayed Riaydh <$email>
2023-06-21 00:07:49 +0600

    feat: styling update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export { default as TestComponent } from "./TestComponent/TestComponent";
-export { default as Button } from "./Button";
-
-import "./styles/styles.scss";
+export { default as TestComponent } from './TestComponent/TestComponent';
+export { default as Button } from './Button';
